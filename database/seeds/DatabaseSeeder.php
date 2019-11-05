6b7059b355048554edeea3210d85913d8b10e063
NoubaaB <bachirnoubaa@hotmail.com>
2019-11-05 23:15:40 +0100

    update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -24,9 +24,9 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         User::create([
-            'name'=>'Admin',
-            'email'=>'RestaurantTazerzit@gmail.com',
-            'password'=>Hash::make('t@zerzit123'),
+            'name' => 'Admin',
+            'email' => 'RestaurantTazerzit@gmail.com',
+            'password' => Hash::make('t@zerzit123'),
         ]);
         Header::create([
             'image' => 'images/bg_1.jpg',
@@ -50,190 +50,190 @@ class DatabaseSeeder extends Seeder
         ]);
 
         Social::create([
-            "nom"=>'facebook',
-            "link"=>'http://www.facebook.com/',
+            "nom" => 'facebook',
+            "link" => 'http://www.facebook.com/',
         ]);
-        
+
         Social::create([
-            "nom"=>'instagram',
-            "link"=>'http://www.instagram.com/',
+            "nom" => 'instagram',
+            "link" => 'http://www.instagram.com/',
         ]);
         Social::create([
-            "nom"=>'hotel',
-            "link"=>'http://www.twitter.com/',
+            "nom" => 'hotel',
+            "link" => 'http://www.twitter.com/',
         ]);
 
         Starter::create([
-            "nom"=>"Cornish - Mackerel",
-            "prix"=>"20.00",
-            "description"=>"tasty and delicious",
-            "image"=>"images/dish-1.jpg",
+            "nom" => "Cornish - Mackerel",
+            "prix" => "20.00",
+            "description" => "tasty and delicious",
+            "image" => "images/dish-1.jpg",
         ]);
         Starter::create([
-            "nom"=>"Roasted Steak",
-            "prix"=>"29.00",
-            "description"=>"tasty and delicious",
-            "image"=>"images/dish-2.jpg",
+            "nom" => "Roasted Steak",
+            "prix" => "29.00",
+            "description" => "tasty and delicious",
+            "image" => "images/dish-2.jpg",
         ]);
         Starter::create([
-            "nom"=>"Seasonal Soup",
-            "prix"=>"20.00",
-            "description"=>"tasty and delicious",
-            "image"=>"images/dish-3.jpg",
+            "nom" => "Seasonal Soup",
+            "prix" => "20.00",
+            "description" => "tasty and delicious",
+            "image" => "images/dish-3.jpg",
         ]);
         Starter::create([
-            "nom"=>"Chicken Curry",
-            "prix"=>"20.00",
-            "description"=>"tasty and delicious",
-            "image"=>"images/dish-4.jpg",
+            "nom" => "Chicken Curry",
+            "prix" => "20.00",
+            "description" => "tasty and delicious",
+            "image" => "images/dish-4.jpg",
         ]);
 
 
         Maindish::create([
-            "nom"=>"Sea Trout",
-            "prix"=>"49.91",
-            "description"=>"tasty and delicious",
-            "image"=>"images/dish-5.jpg",
+            "nom" => "Sea Trout",
+            "prix" => "49.91",
+            "description" => "tasty and delicious",
+            "image" => "images/dish-5.jpg",
         ]);
         Maindish::create([
-            "nom"=>"Roasted Beef",
-            "prix"=>"20.00",
-            "description"=>"tasty and delicious",
-            "image"=>"images/dish-6.jpg",
+            "nom" => "Roasted Beef",
+            "prix" => "20.00",
+            "description" => "tasty and delicious",
+            "image" => "images/dish-6.jpg",
         ]);
         Maindish::create([
-            "nom"=>"Butter Fried Chicken",
-            "prix"=>"20.00",
-            "description"=>"tasty and delicious",
-            "image"=>"images/dish-7.jpg",
+            "nom" => "Butter Fried Chicken",
+            "prix" => "20.00",
+            "description" => "tasty and delicious",
+            "image" => "images/dish-7.jpg",
         ]);
         Maindish::create([
-            "nom"=>"Chiken Filet",
-            "prix"=>"20.00",
-            "description"=>"tasty and delicious",
-            "image"=>"images/dish-8.jpg",
+            "nom" => "Chiken Filet",
+            "prix" => "20.00",
+            "description" => "tasty and delicious",
+            "image" => "images/dish-8.jpg",
         ]);
 
 
         Dessert::create([
-            "nom"=>"Cornish - Mackerel",
-            "prix"=>"20.00",
-            "description"=>"tasty and delicious",
-            "image"=>"images/dessert-1.jpg",
+            "nom" => "Cornish - Mackerel",
+            "prix" => "20.00",
+            "description" => "tasty and delicious",
+            "image" => "images/dessert-1.jpg",
         ]);
         Dessert::create([
-            "nom"=>"Roasted Steak",
-            "prix"=>"29.00",
-            "description"=>"tasty and delicious",
-            "image"=>"images/dessert-2.jpg",
+            "nom" => "Roasted Steak",
+            "prix" => "29.00",
+            "description" => "tasty and delicious",
+            "image" => "images/dessert-2.jpg",
         ]);
         Dessert::create([
-            "nom"=>"Seasonal Soup",
-            "prix"=>"20.00",
-            "description"=>"tasty and delicious",
-            "image"=>"images/dessert-3.jpg",
+            "nom" => "Seasonal Soup",
+            "prix" => "20.00",
+            "description" => "tasty and delicious",
+            "image" => "images/dessert-3.jpg",
         ]);
         Dessert::create([
-            "nom"=>"Chicken Curry",
-            "prix"=>"20.00",
-            "description"=>"tasty and delicious",
-            "image"=>"images/dessert-4.jpg",
+            "nom" => "Chicken Curry",
+            "prix" => "20.00",
+            "description" => "tasty and delicious",
+            "image" => "images/dessert-4.jpg",
         ]);
 
 
         Drink::create([
-            "nom"=>"Sea Trout",
-            "prix"=>"49.91",
-            "description"=>"tasty and delicious",
-            "image"=>"images/drink-5.jpg",
+            "nom" => "Sea Trout",
+            "prix" => "49.91",
+            "description" => "tasty and delicious",
+            "image" => "images/drink-5.jpg",
         ]);
         Drink::create([
-            "nom"=>"Roasted Beef",
-            "prix"=>"20.00",
-            "description"=>"tasty and delicious",
-            "image"=>"images/drink-6.jpg",
+            "nom" => "Roasted Beef",
+            "prix" => "20.00",
+            "description" => "tasty and delicious",
+            "image" => "images/drink-6.jpg",
         ]);
         Drink::create([
-            "nom"=>"Butter Fried Chicken",
-            "prix"=>"20.00",
-            "description"=>"tasty and delicious",
-            "image"=>"images/drink-7.jpg",
+            "nom" => "Butter Fried Chicken",
+            "prix" => "20.00",
+            "description" => "tasty and delicious",
+            "image" => "images/drink-7.jpg",
         ]);
         Drink::create([
-            "nom"=>"Chiken Filet",
-            "prix"=>"20.00",
-            "description"=>"tasty and delicious",
-            "image"=>"images/drink-8.jpg",
+            "nom" => "Chiken Filet",
+            "prix" => "20.00",
+            "description" => "tasty and delicious",
+            "image" => "images/drink-8.jpg",
         ]);
         Drink::create([
-            "nom"=>"Coffe",
-            "prix"=>"20.00",
-            "description"=>"Chocolate Coffe",
-            "image"=>"images/menu-1.jpg",
+            "nom" => "Coffe",
+            "prix" => "20.00",
+            "description" => "Chocolate Coffe",
+            "image" => "images/menu-1.jpg",
         ]);
         Drink::create([
-            "nom"=>"Coffe",
-            "prix"=>"20.00",
-            "description"=>"Chocolate Coffe",
-            "image"=>"images/menu-2.jpg",
+            "nom" => "Coffe",
+            "prix" => "20.00",
+            "description" => "Chocolate Coffe",
+            "image" => "images/menu-2.jpg",
         ]);
         Drink::create([
-            "nom"=>"Coffe",
-            "prix"=>"20.00",
-            "description"=>"Chocolate Coffe",
-            "image"=>"images/menu-3.jpg",
+            "nom" => "Coffe",
+            "prix" => "20.00",
+            "description" => "Chocolate Coffe",
+            "image" => "images/menu-3.jpg",
         ]);
         Drink::create([
-            "nom"=>"Coffe",
-            "prix"=>"20.00",
-            "description"=>"Chocolate Coffe",
-            "image"=>"images/menu-4.jpg",
+            "nom" => "Coffe",
+            "prix" => "20.00",
+            "description" => "Chocolate Coffe",
+            "image" => "images/menu-4.jpg",
         ]);
-        
+
         Table::create([
-            'fname'=>'first name',
-            'lname'=>'last name',
-            'date'=>date("Y/m/d"),
-            'time'=>"08:00pm",
-            'phone'=>'0625483621',
-            'message'=>"lorim long test message",
+            'fname' => 'first name',
+            'lname' => 'last name',
+            'date' => date("Y/m/d"),
+            'time' => "08:00pm",
+            'phone' => '0625483621',
+            'message' => "lorim long test message",
         ]);
 
         //create Hostel FIRST
-      factory(\App\Hostel::class,1)->create(['image'=>'images/181.JPG']);
-      factory(\App\Hostel::class,1)->create(['image'=>'images/171.JPG']);
-      factory(\App\Hostel::class,1)->create(['image'=>'images/096.JPG']);
-      factory(\App\Hostel::class,1)->create(['image'=>'images/172.JPG']);
+        factory(\App\Hostel::class, 1)->create(['image' => 'images/181.JPG']);
+        factory(\App\Hostel::class, 1)->create(['image' => 'images/171.JPG']);
+        factory(\App\Hostel::class, 1)->create(['image' => 'images/096.JPG']);
+        factory(\App\Hostel::class, 1)->create(['image' => 'images/172.JPG']);
 
-      factory(\App\Activitie::class,5)->create(['hostel_id'=>1]);
-      factory(\App\Activitie::class,5)->create(['hostel_id'=>2]);
-      factory(\App\Activitie::class,5)->create(['hostel_id'=>3]);
-      factory(\App\Activitie::class,5)->create(['hostel_id'=>4]);
+        factory(\App\Activitie::class, 5)->create(['hostel_id' => 1]);
+        factory(\App\Activitie::class, 5)->create(['hostel_id' => 2]);
+        factory(\App\Activitie::class, 5)->create(['hostel_id' => 3]);
+        factory(\App\Activitie::class, 5)->create(['hostel_id' => 4]);
 
-      factory(\App\Image::class,1)->create(['hostel_id'=>1,'image'=>'images/181.JPG']);
-      factory(\App\Image::class,1)->create(['hostel_id'=>1,'image'=>'images/177.JPG']);
+        factory(\App\Image::class, 1)->create(['hostel_id' => 1, 'image' => 'images/181.JPG']);
+        factory(\App\Image::class, 1)->create(['hostel_id' => 1, 'image' => 'images/177.JPG']);
 
-      factory(\App\Image::class,1)->create(['hostel_id'=>2,'image'=>'images/171.JPG']);
-      factory(\App\Image::class,1)->create(['hostel_id'=>2,'image'=>'images/172.JPG']);
-      factory(\App\Image::class,1)->create(['hostel_id'=>2,'image'=>'images/176.JPG']);
+        factory(\App\Image::class, 1)->create(['hostel_id' => 2, 'image' => 'images/171.JPG']);
+        factory(\App\Image::class, 1)->create(['hostel_id' => 2, 'image' => 'images/172.JPG']);
+        factory(\App\Image::class, 1)->create(['hostel_id' => 2, 'image' => 'images/176.JPG']);
 
-      factory(\App\Image::class,1)->create(['hostel_id'=>3,'image'=>'images/096.JPG']);
-      factory(\App\Image::class,1)->create(['hostel_id'=>3,'image'=>'images/110.JPG']);
-      factory(\App\Image::class,1)->create(['hostel_id'=>3,'image'=>'images/anas.JPG']);
-      
-      factory(\App\Image::class,3)->create(['hostel_id'=>4,'image'=>'images/172.JPG']);
+        factory(\App\Image::class, 1)->create(['hostel_id' => 3, 'image' => 'images/096.JPG']);
+        factory(\App\Image::class, 1)->create(['hostel_id' => 3, 'image' => 'images/110.JPG']);
+        factory(\App\Image::class, 1)->create(['hostel_id' => 3, 'image' => 'images/anas.JPG']);
 
-      Info::create([
-          'phone'=>'+212 649-8479968',
-          'email'=>'Tazerzit@gmail.com',
-          'adress'=>'Restaurant Tazerzit, Taghazout, Morocco (MA)',
-          'openDays'=>'Open All Days',
-          'time'=>'8:00am - 12:00pm',
-          'phoneText'=>'Hostel, Beach & Surfing Schools, Check our Hostel Packs for More Info and Booking',
-          'menu'=>"Moroccan cuisine is the culinary star of North Africa. Imperial and trade influence has been filtered and blended into Morocco's culture. Being at the crossroads of many civilizations, the cuisine of Morocco is a mélange of Arab, Berber, Moorish, French, Middle Eastern, Mediterranean African, Iberian, and Jewish influences. Moroccan cooking is enhanced with fruits, dried and fresh -- apricots, dates, figs, and raisins, to name a few. Lemons preserved in a salt-lemon juice mixture bring a unique face to many Moroccan chicken and pigeon dishes. Nuts are prominent; pine nuts, almonds, and pistachios show up in all sorts of unexpected places. Moroccan sweets are rich and dense confections of cinnamon, almond, and fruit perfumes that are rolled in filo dough, soaked in honey, and stirred into puddings.",
-          'story'=>'A few kilometres away from Agadir, there is a beach with a small islet known to all surfers as Devil’s rock. When you ask surfers about it, they would tell you it is a safe spot to learn and improve surfing levels. When you ask locals, it is a whole other story! An old Superstition passed down from generation to generation is still around, and devil’s Rock is not only a surf spot; it holds a mind-boggling belief..',
-          'location'=>'Restaurant Tazerzit , Taghazout, Morocco',
-          'about'=>'A few kilometres away from Agadir, there is a beach with a small islet known to all surfers as Devil’s rock. When you ask surfers about it, they would tell you it is a safe spot to learn and improve surfing levels.',
-      ]);
+        factory(\App\Image::class, 3)->create(['hostel_id' => 4, 'image' => 'images/172.JPG']);
+
+        Info::create([
+            'phone' => '+212 613128843',
+            'email' => 'Tazerzit@gmail.com',
+            'adress' => 'Restaurant Tazerzit, Taghazout, Morocco (MA)',
+            'openDays' => 'Open All Days',
+            'time' => '8:00am - 12:00pm',
+            'phoneText' => 'Hostel, Beach & Surfing Schools, Check our Hostel Packs for More Info and Booking',
+            'menu' => "Moroccan cuisine is the culinary star of North Africa. Imperial and trade influence has been filtered and blended into Morocco's culture. Being at the crossroads of many civilizations, the cuisine of Morocco is a mélange of Arab, Berber, Moorish, French, Middle Eastern, Mediterranean African, Iberian, and Jewish influences. Moroccan cooking is enhanced with fruits, dried and fresh -- apricots, dates, figs, and raisins, to name a few. Lemons preserved in a salt-lemon juice mixture bring a unique face to many Moroccan chicken and pigeon dishes. Nuts are prominent; pine nuts, almonds, and pistachios show up in all sorts of unexpected places. Moroccan sweets are rich and dense confections of cinnamon, almond, and fruit perfumes that are rolled in filo dough, soaked in honey, and stirred into puddings.",
+            'story' => 'A few kilometres away from Agadir, there is a beach with a small islet known to all surfers as Devil’s rock. When you ask surfers about it, they would tell you it is a safe spot to learn and improve surfing levels. When you ask locals, it is a whole other story! An old Superstition passed down from generation to generation is still around, and devil’s Rock is not only a surf spot; it holds a mind-boggling belief..',
+            'location' => 'Restaurant Tazerzit , Taghazout, Morocco',
+            'about' => 'A few kilometres away from Agadir, there is a beach with a small islet known to all surfers as Devil’s rock. When you ask surfers about it, they would tell you it is a safe spot to learn and improve surfing levels.',
+        ]);
     }
 }
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -41,6 +41,7 @@
   <link rel="stylesheet" href="{{asset('css/css/flaticon.css')}}">
   <link rel="stylesheet" href="{{asset('css/css/icomoon.css')}}">
   <link rel="stylesheet" href="{{asset('css/css/style.css')}}">
+  <link rel="stylesheet" href="{{asset('css/css/whatsapp.css')}}">
 
   <script src="https://www.google.com/recaptcha/api.js" async defer></script>
   <style>
@@ -290,8 +291,39 @@
       </div>
     </div>
   </footer>
+  <!-- partial:index.partial.html -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+  <div class="nav-bottom">
+    <div class="popup-whatsapp fadeIn">
+      <div class="content-whatsapp -top"><button type="button" class="closePopup">
+          <i class="icon icon-close">
+
+          </i>
+        </button>
+        <p>Hello, 😊 need help?</p>
+      </div>
+      <div class="content-whatsapp -bottom">
+        <input class="whats-input" id="whats-in" type="text" Placeholder="Send message..." />
+        <button class="send-msPopup" id="send-btn" type="button">
+          <i class="icon icon-send">
+
+          </i>
+        </button>
+
+      </div>
+    </div>
+    <button type="button" id="whats-openPopup" class="whatsapp-button">
+      <img class="icon-whatsapp" src="https://image.flaticon.com/icons/svg/134/134937.svg">
+    </button>
+    <div class="circle-anime"></div>
+  </div>
+  <!-- partial -->
+
 
   <script>
+    var phoneNumber = '';
+
     new Vue({
       el: '#social',
       data: {
@@ -344,7 +376,8 @@
         getInfos: function() {
           axios.get("{{route('info.index')}}").then((response) => {
             this.info = response.data;
-            console.log(this.info);
+            phoneNumber = this.info.phone.split('+', 2)[1];
+            console.log(phoneNumber);
           }).catch((errors) => {
             console.log(errors);
           });
@@ -396,9 +429,38 @@
         this.getInfos();
         this.getInstagram_Collection();
       }
-
-
     });
+    // another script
+    popupWhatsApp = () => {
+
+      let btnClosePopup = document.querySelector('.closePopup');
+      let btnOpenPopup = document.querySelector('.whatsapp-button');
+      let popup = document.querySelector('.popup-whatsapp');
+      let sendBtn = document.getElementById('send-btn');
+
+      btnClosePopup.addEventListener("click", () => {
+        popup.classList.toggle('is-active-whatsapp-popup')
+      })
+
+      btnOpenPopup.addEventListener("click", () => {
+        popup.classList.toggle('is-active-whatsapp-popup')
+        popup.style.animation = "fadeIn .6s 0.0s both";
+      })
+
+      sendBtn.addEventListener("click", () => {
+        let msg = document.getElementById('whats-in').value;
+        let relmsg = msg.replace(/ /g, "%20");
+        //just change the numbers "1515551234567" for your number. Don't use +001-(555)1234567     
+        window.open(`https://wa.me/${phoneNumber}?text=` + relmsg, '_blank');
+
+      });
+
+      setTimeout(() => {
+        popup.classList.toggle('is-active-whatsapp-popup');
+      }, 3000);
+    }
+
+    popupWhatsApp();
   </script>
 
 
@@ -426,6 +488,7 @@
   <script src="{{asset('js/scrollax.min.js')}}"></script>
   <script src="{{asset('js/main.js')}}"></script>
   <script src="{{asset('js/sweetAtert.js')}}"></script>
+
   @csrf
 </body>
 
